2d0d126812ee23bf7aaa5238f0583116339b0724
bman-prog <bmankovski2028@communityschoolnaples.org>
2025-03-24 11:14:13 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-star_sprites = []
+star_sprites : List[Sprite] = []
 
-for star in range(10):
-    sta = sprites.create(img("""
-        . . . . . . . b b . . . . . . .
-        . . . . . . b d d b . . . . . .
-        . . . . . b d 5 5 d b . . . . .
-        . . . . b b 5 5 5 5 b b . . . .
-        . . . . b 5 5 5 5 5 5 b . . . .
-        b b b b b 5 5 5 5 1 1 d b b b b
-        b 5 5 5 5 5 5 5 5 1 1 1 5 5 5 b
-        b d d 5 5 5 5 5 5 1 1 1 5 d d b
-        . b d d 5 5 5 5 5 5 5 5 d d b .
-        . . b b 5 5 5 5 5 5 5 5 b b . .
-        . . c b 5 5 5 5 5 5 5 5 b c . .
-        . . c 5 5 5 5 d d 5 5 5 5 c . .
-        . . c 5 5 d b b b b d 5 5 c . .
-        . . c 5 d b c c c c b d 5 c . .
-        . . c c c c . . . . c c c c . .
-        . . . . . . . . . . . . . . . .
-    """), SpriteKind.player)
-    sta.set_position(randint(0, 120), randint(0, 120))
-    star_sprites.append(star)
-    pause(100)
+
+
+while True:
+    for i in range(10):
+                sta = sprites.create(img("""
+                    . . . . . . . b b . . . . . . .
+                    . . . . . . b d d b . . . . . .
+                    . . . . . b d 5 5 d b . . . . .
+                    . . . . b b 5 5 5 5 b b . . . .
+                    . . . . b 5 5 5 5 5 5 b . . . .
+                    b b b b b 5 5 5 5 1 1 d b b b b
+                    b 5 5 5 5 5 5 5 5 1 1 1 5 5 5 b
+                    b d d 5 5 5 5 5 5 1 1 1 5 d d b
+                    . b d d 5 5 5 5 5 5 5 5 d d b .
+                    . . b b 5 5 5 5 5 5 5 5 b b . .
+                    . . c b 5 5 5 5 5 5 5 5 b c . .
+                    . . c 5 5 5 5 d d 5 5 5 5 c . .
+                    . . c 5 5 d b b b b d 5 5 c . .
+                    . . c 5 d b c c c c b d 5 c . .
+                    . . c c c c . . . . c c c c . .
+                    . . . . . . . . . . . . . . . .
+                """), SpriteKind.player)
+                sta.set_position(randint(0, 120), randint(0, 120))
+                star_sprites.append(sta)
+                pause(100)
+    for star in star_sprites:
+        pause(100)
+        star.set_flag(SpriteFlag.INVISIBLE, True)
+        
+
+
+    
+
 
     
 
--- main.ts
+++ main.ts
@@ -1,25 +1,31 @@
 let sta: Sprite;
-let star_sprites = []
-for (let star = 0; star < 10; star++) {
-    sta = sprites.create(img`
-        . . . . . . . b b . . . . . . .
-        . . . . . . b d d b . . . . . .
-        . . . . . b d 5 5 d b . . . . .
-        . . . . b b 5 5 5 5 b b . . . .
-        . . . . b 5 5 5 5 5 5 b . . . .
-        b b b b b 5 5 5 5 1 1 d b b b b
-        b 5 5 5 5 5 5 5 5 1 1 1 5 5 5 b
-        b d d 5 5 5 5 5 5 1 1 1 5 d d b
-        . b d d 5 5 5 5 5 5 5 5 d d b .
-        . . b b 5 5 5 5 5 5 5 5 b b . .
-        . . c b 5 5 5 5 5 5 5 5 b c . .
-        . . c 5 5 5 5 d d 5 5 5 5 c . .
-        . . c 5 5 d b b b b d 5 5 c . .
-        . . c 5 d b c c c c b d 5 c . .
-        . . c c c c . . . . c c c c . .
-        . . . . . . . . . . . . . . . .
-    `, SpriteKind.Player)
-    sta.setPosition(randint(0, 120), randint(0, 120))
-    star_sprites.push(star)
-    pause(100)
+let star_sprites : Sprite[] = []
+while (true) {
+    for (let i = 0; i < 10; i++) {
+        sta = sprites.create(img`
+                    . . . . . . . b b . . . . . . .
+                    . . . . . . b d d b . . . . . .
+                    . . . . . b d 5 5 d b . . . . .
+                    . . . . b b 5 5 5 5 b b . . . .
+                    . . . . b 5 5 5 5 5 5 b . . . .
+                    b b b b b 5 5 5 5 1 1 d b b b b
+                    b 5 5 5 5 5 5 5 5 1 1 1 5 5 5 b
+                    b d d 5 5 5 5 5 5 1 1 1 5 d d b
+                    . b d d 5 5 5 5 5 5 5 5 d d b .
+                    . . b b 5 5 5 5 5 5 5 5 b b . .
+                    . . c b 5 5 5 5 5 5 5 5 b c . .
+                    . . c 5 5 5 5 d d 5 5 5 5 c . .
+                    . . c 5 5 d b b b b d 5 5 c . .
+                    . . c 5 d b c c c c b d 5 c . .
+                    . . c c c c . . . . c c c c . .
+                    . . . . . . . . . . . . . . . .
+                `, SpriteKind.Player)
+        sta.setPosition(randint(0, 120), randint(0, 120))
+        star_sprites.push(sta)
+        pause(100)
+    }
+    for (let star of star_sprites) {
+        pause(100)
+        star.setFlag(SpriteFlag.Invisible, true)
+    }
 }
